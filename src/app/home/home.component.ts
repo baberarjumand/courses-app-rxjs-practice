d9ff0dc8d55cd6cadc6405fdcb3a316d13127732
baberarjumand <baber.arjumand@gmail.com>
2020-04-08 12:34:08 +0300

    completed exercise 2.4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -61,8 +61,68 @@ export class HomeComponent implements OnInit {
     // we will implement the reactive design approach to our home component in this lesson
     // we want to define two streams of data, one for beginner and advanced courses
     // we will derive those two streams using rxjs operators
+    // const http$ = createHttpObservable("api/courses");
+    // const courses$: Observable<Course[]> = http$.pipe(
+    //   map((res) => Object.values(res["payload"]))
+    // );
+
+    // this.beginnerCourses$ = courses$.pipe(
+    //   map((courses) =>
+    //     courses.filter((course) => course.category == "BEGINNER")
+    //   )
+    // );
+
+    // this.advancedCourses$ = courses$.pipe(
+    //   map((courses) =>
+    //     courses.filter((course) => course.category == "ADVANCED")
+    //   )
+    // );
+
+    // async pipe to be used with observables in html template
+    // async pipe also directly unsubscribes when destroyed
+    // async means subscriptions are handled at template level, we do not need to subscribe
+    // this implementation is an example of reactive design
+    // by handling the subscribe part in templates, we avoid nested subscribe calls at component level
+    // however, in this implementation, we are making 2 http back-end requests instead of 1 previously
+    // will look at another rxjs operator in next video that can help with this issue
+    // end of video 2.3
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // start of video 2.4
+
+    // when we changed our code to conform to a reactive/declarative approach, we caused another issue
+    // our code is now making 2 http requests, one for beginner courses and another for advanced
+    // this is inefficient compared to the 1 request in our previous imperative approach
+    // we can solve this problem by using the shareReplay() rxjs operator
+
+    // upon investigating the http response on browser dev tools, both http reqs are fetching the same data
+    // 2 subscriptions trigger 2 separate http requests
+    // if 3 subscriptions are made on the courses$ observable, there will be 3 http calls
+    // making multiple http requests to fetch the same data is undesirable and inefficient
+    // the shareReplay() rxjs operator will help us tackle this issue
+
+    // we want to share courses$ stream across multiple subscribers
+    // we want to avoid the default observable behavior which creates a new stream on every new subscription
+    // we want the courses$ stream to be executed only once
+    // then we want the result of this stream to be REPLAYED to each new subscriber
+
+    // to display the modified behavior with the shareReplay() operator, we will log info to console
+    // to output to console, we will use the tap() operator
+    // tap() is used to produce 'side-effects' in our observable shade
+    // tap() is used to make changes/execute code unrelated to the current observable
+    // for example, changing some other variable in the component, or outputting to console
+
     const http$ = createHttpObservable("api/courses");
-    const courses$: Observable<Course[]> = http$.pipe(map((res) => Object.values(res["payload"])));
+    const courses$: Observable<Course[]> = http$.pipe(
+      tap(() => {
+        console.log("HTTP Request executed!");
+      }),
+      map((res) => Object.values(res["payload"])),
+      shareReplay()
+    );
+
+    // testing if indeed there are no extra http reqs after shareReplay() operator is used
+    // courses$.subscribe();
 
     this.beginnerCourses$ = courses$.pipe(
       map((courses) =>
@@ -76,15 +136,8 @@ export class HomeComponent implements OnInit {
       )
     );
 
-    // async pipe to be used with observables in html template
-    // async pipe also directly unsubscribes when destroyed
-    // async means subscriptions are handled at template level, we do not need to subscribe
-    // this implementation is an example of reactive design
-    // by handling the subscribe part in templates, we avoid nested subscribe calls at component level
-    // however, in this implementation, we are making 2 http back-end requests instead of 1 previously
-    // will look at another rxjs operator in next video that can help with this issue
-
-    // end of video 2.3
+    // shareReplay() operator is very commonly used for handling http requests
+    // end of video 2.4
     ////////////////////////////////////////////////////////////////////////////////////////////
   }
 }
