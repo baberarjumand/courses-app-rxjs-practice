4f8352580fe09565418fb68ebe9f28e6ede24b68
baberarjumand <baber.arjumand@gmail.com>
2020-04-09 13:14:45 +0300

    completed exercise 2.15

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -32,19 +32,20 @@ import { createHttpObservable } from "../common/util";
 export class CourseComponent implements OnInit, AfterViewInit {
   course$: Observable<Course>;
   lessons$: Observable<Lesson[]>;
+  courseId: string;
 
   @ViewChild("searchInput", { static: true }) input: ElementRef;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    const courseId = this.route.snapshot.params["id"];
+    this.courseId = this.route.snapshot.params["id"];
     ////////////////////////////////////////////////////////////////////////////////////////////
     // start of video 2.13
-    this.course$ = createHttpObservable(`/api/courses/${courseId}`);
-    this.lessons$ = createHttpObservable(
-      `/api/lessons?courseId=${courseId}&pageSize=100`
-    ).pipe(map((res) => res["payload"]));
+    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+
+    // this line was moved in video 2.15
+    // this.lessons$ = this.loadLessons();
 
     // in the next lesson, we will implement the type-ahead search logic using switchMap()
     // and other operators as well
@@ -86,18 +87,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
     //   )
     //   .subscribe(console.log);
 
-    // we also need to solve the problem of duplicate values being emitted
-    // we want to derive a new observable from the observable above that omits the duplicates
-    // if two consecutive values are exactly the same, we only want to emit one value
-    // to do that, we use the distinctUntilChanged() operator
-    // with this operator, we will no longer have duplicate values in our output
-    fromEvent<any>(this.input.nativeElement, "keyup")
-      .pipe(
-        map((event) => event.target.value),
-        debounceTime(400),
-        distinctUntilChanged()
-      )
-      .subscribe(console.log);
+    // // we also need to solve the problem of duplicate values being emitted
+    // // we want to derive a new observable from the observable above that omits the duplicates
+    // // if two consecutive values are exactly the same, we only want to emit one value
+    // // to do that, we use the distinctUntilChanged() operator
+    // // with this operator, we will no longer have duplicate values in our output
+    // fromEvent<any>(this.input.nativeElement, "keyup")
+    //   .pipe(
+    //     map((event) => event.target.value),
+    //     debounceTime(400),
+    //     distinctUntilChanged()
+    //   )
+    //   .subscribe(console.log);
 
     // in the next lesson, we will decide what is the best operator to convert this search term to
     // a backend request. we will see concatMap(), mergeMap() and exhaustMap() are not a good
@@ -105,5 +106,77 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     // end of video 2.14
     ////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // start of video 2.15
+
+    // in this lesson we will transform our search strings to backend reqs
+    // we will load the lessons according to the search term
+
+    // // let us first see why concatMap() is not a good choice
+    // fromEvent<any>(this.input.nativeElement, "keyup")
+    //   .pipe(
+    //     map((event) => event.target.value),
+    //     debounceTime(400),
+    //     distinctUntilChanged(),
+    //     concatMap((search) => this.loadLessons(search))
+    //   )
+    //   .subscribe(console.log);
+    // with concatMap(), the current search request does not get cancelled if search string changes
+    // we do get multiple distinct results, but the previous reqs complete
+    // we want to cancel the previous http req if user changes the search string
+    // and then emit the new search req with the latest search string
+    // switchMap() operator will help us accomplish this
+    // refer to official doc for switchMap() @ reactivex.io
+
+    // // with switchMap(), if the source observable emits a new value,
+    // // then the currently subscribed observable gets unsubscribed,
+    // // and it SWITCHES to the new observable and starts emitting it's values
+    // // switchMap() is all about unsubscribe-logic
+
+    // fromEvent<any>(this.input.nativeElement, "keyup")
+    //   .pipe(
+    //     map((event) => event.target.value),
+    //     debounceTime(400),
+    //     distinctUntilChanged(),
+    //     switchMap((search) => this.loadLessons(search))
+    //   )
+    //   .subscribe(console.log);
+
+    // you can observe cancelled http reqs in the browser dev tools
+    // we are not yet displaying the search results, so we will implement that next
+
+    // currently we have a lessons$ observable that is emitted to show the current table of lessons
+    // we will convert this lessons$ to a combination to 2 different observables
+    // we want it to initially load all the lessons, that will be the initialLessons$ observable
+    // next, when the user starts typing in search box, we want to switch the output of
+    // the second stream which shows the results of the search string
+    // we want to combine 2 different observables, concatenation will help us here
+    // we want the first initial observable to get loaded, only then should we
+    // start responding to the search-strings/user-searches
+
+    const searchLessons$ = fromEvent<any>(
+      this.input.nativeElement,
+      "keyup"
+    ).pipe(
+      map((event) => event.target.value),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((search) => this.loadLessons(search))
+    );
+
+    const initialLessons$ = this.loadLessons();
+
+    this.lessons$ = concat(initialLessons$, searchLessons$);
+
+    // in the next lessons, we will cover rxjs error handling
+
+    // end of video 2.15
+    ////////////////////////////////////////////////////////////////////////////////////////////
+  }
+
+  loadLessons(search = ""): Observable<Lesson[]> {
+    return createHttpObservable(
+      `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`
+    ).pipe(map((res) => res["payload"]));
   }
 }
