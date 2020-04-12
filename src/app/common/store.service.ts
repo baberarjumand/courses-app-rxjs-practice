b6714c6b10ef2851a9990e2f2da863d15400eb1a
baberarjumand <baber.arjumand@gmail.com>
2020-04-12 12:36:41 +0300

    completed exercise 5.5, added some notes

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -9,6 +9,13 @@ export class Store {
   ///////////////////////////////////////////////////////////////////
   // start of video 5.5
 
+  // as of now, every time we navigate away from the homepage and back,
+  //  a http req gets made to fetch the courses
+  // this is inefficient because most of the time the same data
+  //  is being fetched
+  // we will use this Store service to fetch the data once, and make it
+  //  available to the whole application
+
   // we want to make sure that any subscriber gets the latest
   //  result from the back-end, so we use BehaviorSubject
 
@@ -16,5 +23,9 @@ export class Store {
   courses$: Observable<Course[]> = this.subject.asObservable();
 
   // end of video 5.5
+  ///////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  // start of video 5.6
+  // end of video 5.6
   ///////////////////////////////////////////////////////////////////
 }
